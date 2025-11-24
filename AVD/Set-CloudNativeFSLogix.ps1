75e95d2ee60553ee01475119de19ad930180f2ab
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2025-11-23 18:28:16 -0600

    Modify profile path and add resource links
    Updated the profile path placeholder and added references to external resources.

--- AVD/Set-CloudNativeFSLogix.ps1
+++ AVD/Set-CloudNativeFSLogix.ps1
@@ -1,11 +1,12 @@
-# Code used for AVD and FSLogix with Entra-only Azure Files
+# Code used for AVD and FSLogix with Entra-only Azure Files https://youtu.be/SHHP2ZoFBD4
 
 # Configure the clients to retrieve Kerberos tickets
+# https://learn.microsoft.com/en-us/azure/storage/files/storage-files-identity-auth-hybrid-identities-enable?tabs=azure-portal%2Cregkey&WT.mc_id=AZ-MVP-5004159#configure-the-clients-to-retrieve-kerberos-tickets
 reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters /v CloudKerberosTicketRetrievalEnabled /t REG_DWORD /d 1
 
 
 # Registry Settings for FSLogix
-$profilePath = "\\entrastrgdemo1991.file.core.windows.net\entrafslogix\profiles"  # Replace with your Azure File Share path
+$profilePath = "\\<storage_account..file.core.windows.net\<share>\<directory-optional>"  # Replace with your Azure File Share path
 Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profilePath
 Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -Type DWord
 Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -Type DWord
