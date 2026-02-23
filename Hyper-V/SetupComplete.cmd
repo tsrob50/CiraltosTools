7483daca95522a52ac7c8c2cdc26a8a9a3ea61c6
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2026-02-23 17:20:18 -0600

    Create SetupComplete.cmd to execute PowerShell script
    Windows startup .cmd script to run PowerShell commands after the first boot.

---
+++ Hyper-V/SetupComplete.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM Run your PowerShell script.
+REM Update the path to your PowerShell script as needed.
+powershell.exe -ExecutionPolicy Bypass -File C:\Windows\Setup\Scripts\Update-Client.ps1
+REM Add other system configuration commands here.
